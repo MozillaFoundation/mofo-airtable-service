7c6e5d379bbd033b011b76ce8af3934ce0c70479
Gideon Thomas <gideon@mozillafoundation.org>
2019-05-13 18:55:08 -0400

    Test without auth

--- api/auth.js
+++ api/auth.js
@@ -3,17 +3,17 @@ const Habitat = require("habitat");
 const API_KEY = (new Habitat()).get("apiKey");
 
 module.exports = function(req, res, next) {
-  const auth = req.get("authorization");
+//   const auth = req.get("authorization");
   
-  console.log('All headers: ', req.headers);
+//   console.log('All headers: ', req.headers);
 
-  if (!auth) {
-    return res.sendStatus(401);
-  }
+//   if (!auth) {
+//     return res.sendStatus(401);
+//   }
 
-  if (auth.indexOf("Basic ") === 0 && auth.slice(6) === API_KEY) {
+//   if (auth.indexOf("Basic ") === 0 && auth.slice(6) === API_KEY) {
     next();
-  } else {
-    res.sendStatus(403);
-  }
+//   } else {
+//     res.sendStatus(403);
+//   }
 };
